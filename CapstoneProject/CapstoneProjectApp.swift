71f18a2077a25a75a1925a9c0c5c8d86da7cba94
Hannah Denaer <denaerhannah@gmail.com>
2025-05-16 17:21:52 -0500

    Initial Commit

---
+++ CapstoneProject/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject
+//
+//  Created by Scholar on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
